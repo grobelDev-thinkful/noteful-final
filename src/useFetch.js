0c3aec984383c93f7158fe59770eedf9e5f1a0b6
grobelDev <grobeldev@gmail.com>
2019-11-18 13:28:03 -0800

    deleted redundant file

--- src/useFetch.js
+++
@@ -1,9 +0,0 @@
-import { useEffect, useState } from "react";
-
-export default function useFetch(url) {
-  const [state, setState] = useState({ folders: null, notes: null });
-
-  useEffect(() => {
-    // setState(state => )
-  });
-}
